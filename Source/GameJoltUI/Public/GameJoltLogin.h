de5a0f5fb5740eaee6bc26fd2241568e094c66ea
FreezerNick <nl@f2p-entertainment.de>
2020-10-06 11:18:14 +0200

    Add LoginWidget Class

---
+++ Source/GameJoltUI/Public/GameJoltLogin.h
@@ -0,0 +1,17 @@
+// Copyright by Free2Play-Entertainment (2020)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GameJoltLogin.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJOLTUI_API UGameJoltLogin : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
